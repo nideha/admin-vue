d4e9b88fee62ef575eb046617594b4328d38170e
Lixingyang <2414851878@qq.com>
2018-04-13 22:52:41 +0800

    导入element组件库并简单配置了登录组件

--- src/components/home/home.vue
+++ src/components/home/home.vue
@@ -1,12 +1,15 @@
 <template>
-  <div id="app">
-    <router-view></router-view>
-  </div>
+<div>
+  <p>home component</p>
+</div>
 </template>
 
 <script>
 export default {
-  name: 'App'
+  data () {
+    return {
+    }
+  }
 }
 </script>
 
--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,22 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+// @是src路径的别名，webpack配置的   .vue 的后缀名可以省略
+import Login from '@/components/login/login.vue'
+import Home from '@/components/home/home.vue'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
     {
+      name: 'login',
+      path: '/login',
+      component: Login
+    },
+    {
+      name: 'home',
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      component: Home
     }
   ]
 })
